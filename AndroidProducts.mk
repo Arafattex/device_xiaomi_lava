7c93231df3c39c6731e16052762d91119fc08d66
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-25 11:09:52 +0600

    Create AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/aosp_lava.mk
+   $(LOCAL_DIR)/stag_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lava-user \
-    aosp_lava-userdebug \
-    aosp_lava-eng
+    stag_lava-user \
+    stag_lava-userdebug \
+    stag_lava-eng
